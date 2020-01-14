d32803ca5de30ec27ed0655b49dc5614473e3ea6
Max Reynolds <max@tillysinteriors.com>
2020-01-14 08:17:58 +0000

    Create index.js

---
+++ store/index.js
@@ -0,0 +1,3 @@
+// Store is needed for @nuxt/auth. Creating this file enables it.
+// To use vuex store, edit this file - as long as there is an index.js
+// in the store directory, @nuxt/auth should be happy.
