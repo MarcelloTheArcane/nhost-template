c81ee6a3e53a68a5ba15e5d1216756c100a4731c
Max Reynolds <max@tillysinteriors.com>
2020-11-13 17:28:50 +0000

    Delete index.js

--- store/index.js
+++
@@ -1,3 +0,0 @@
-// Store is needed for @nuxt/auth. Creating this file enables it.
-// To use vuex store, edit this file - as long as there is an index.js
-// in the store directory, @nuxt/auth should be happy.
